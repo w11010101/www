88e1b01a93ab797bec2b7bc1cbef791ada37c666
w11010101 <w11010101@hotmail.com>
2017-11-07 17:41:19 +0800

    backups 备份

--- nodejs/upload/upload.js
+++ nodejs/upload/upload.js
@@ -1,7 +1,35 @@
 var http = require("http");
 var fs = require("fs");
 var express = require("express");
+var app = new express();
 
-console.log(express);
+app.use(express.static("bin"));
+app.use(express.static("test"));
+app.use(express.static("doc"));
+app.use(express.static("lib"));
+
+
+app.get("/upload", function(req,res) {
+    console.log(__dirname + "/test/index.html");
+    res.sendFile(__dirname+"/test/index.html");
+    
+    // console.log(req)
+    // res.send(req.toString());
+    // res.send("1111hello word!");
+    // fs.readFile("./index.html",function(err,data){
+    //     if(err){
+    //         return console.error(err);
+    //     }else{
+    //         console.log(data.path);
+    //     }
+    // })
+})
+
+var server = app.listen("8081",function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
+
 
 
