e90fd8416fcc9846e2e47cf8e2844cd8e00bf31a
w11010101 <w11010101@hotmail.com>
2017-11-09 20:45:35 +0800

    backups 备份

--- nodejs/upload/run.js
+++ nodejs/upload/run.js
@@ -1,38 +1,52 @@
-var http = require("http");
+var express = require('express');
+var app = express();
 var fs = require("fs");
-var express = require("express");
-
-var app = new express();
-
 var bodyParser = require("body-parser");
-multer = require("multer");
+var multer = require("multer");
 
 app.use(express.static("bin"));
 app.use(express.static("test"));
 app.use(express.static("doc"));
 app.use(express.static("lib"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/save/'}).array('image'));
 
 app.get("/upload", function(req,res) {
-    // console.log(__dirname + "/test/index.html");
     res.sendFile(__dirname+"/test/index.html");
-    
-    // console.log(req)
-    // res.send(req.toString());
-    // res.send("1111hello word!");
-    // fs.readFile("./index.html",function(err,data){
-    //     if(err){
-    //         return console.error(err);
-    //     }else{
-    //         console.log(data.path);
-    //     }
-    // })
 })
 
 app.post("/upload_imgs",function (req,res){
-    console.log(req.body);
-    // res.json(req.body);
+    // var f = __dirname + "" + req.body.file[0]
+    console.log(req.files);
+    // console.log(req.files[0]);
+    // console.log(req)
+    // for(var f of req.body.file){
+        // fs.appendFile("doc/"+f, "data to append", function(err) {
+        //     if(err) throw err;
+        //     console.log('The "data to append" was appended to file!');
+        // })
+    // }
+    
 })
+// app.post('/upload_imgs', function (req, res) {
+//     console.log(req.files[0]);  // 上传的文件信息
+//     var des_file = __dirname + "/" + req.files[0].originalname;
+//     fs.readFile(req.files[0].path, function (err, data) {
+//         fs.writeFile(des_file, data, function (err) {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 response = {
+//                     message: 'File uploaded successfully',
+//                     filename: req.files[0].originalname
+//                 };
+//             }
+//             console.log(response);
+//             res.end(JSON.stringify(response));
+//         });
+//     });
+// })
 
 var server = app.listen("8081",function(){
     var host = server.address().address;
