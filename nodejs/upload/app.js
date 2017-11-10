99a2ac0a6424905ec39cbda9a04aa3cb2c4c0636
w11010101 <w11010101@hotmail.com>
2017-11-10 17:44:57 +0800

    backups 备份

--- nodejs/upload/app.js
+++ nodejs/upload/app.js
@@ -7,7 +7,7 @@ var multer = require('multer');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/' }).array('image'));
+app.use(multer({ dest: '/doc/uploaded/' }).array('images'));
 
 app.get("/upload", function (req, res) {
     res.sendFile(__dirname + "/app.html");
@@ -17,22 +17,24 @@ app.get("/upload", function (req, res) {
 app.post('/file_upload', function (req, res) {
 
     console.log(req.files);  // 上传的文件信息
+    for(var f of req.files){
+        var des_file = __dirname + "/" + f.originalname;
+        fs.readFileSync(f.path, function (err, data) {
 
-    var des_file = __dirname + "/" + req.files[0].originalname;
-    fs.readFile(req.files[0].path, function (err, data) {
-        fs.writeFile(des_file, data, function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                response = {
-                    message: 'File uploaded successfully',
-                    filename: req.files[0].originalname
-                };
-            }
-            console.log(response);
-            res.end(JSON.stringify(response));
+            fs.writeFileSync(des_file, data, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    response = {
+                        message: 'File uploaded successfully',
+                        filename: f.originalname
+                    };
+                }
+                console.log(response);
+                res.end(JSON.stringify(response));
+            });
         });
-    });
+     }
 })
 
 var server = app.listen(8081, function () {
--- nodejs/upload/run.js
+++ nodejs/upload/run.js
@@ -1,52 +1,65 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-var bodyParser = require("body-parser");
-var multer = require("multer");
 
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var router = express.Router();
 app.use(express.static("bin"));
 app.use(express.static("test"));
 app.use(express.static("doc"));
 app.use(express.static("lib"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/save/'}).array('image'));
+// app.use(multer({dest: 'doc/uploaded/'}).array('image'));
 
+var storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'doc/uploaded/');
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname);
+    }
+})
+var upload = multer({
+    storage:storage
+})
 app.get("/upload", function(req,res) {
     res.sendFile(__dirname+"/test/index.html");
 })
 
-app.post("/upload_imgs",function (req,res){
-    // var f = __dirname + "" + req.body.file[0]
-    console.log(req.files);
-    // console.log(req.files[0]);
-    // console.log(req)
-    // for(var f of req.body.file){
-        // fs.appendFile("doc/"+f, "data to append", function(err) {
-        //     if(err) throw err;
-        //     console.log('The "data to append" was appended to file!');
-        // })
-    // }
+app.post("/upload_imgs",upload.array('image'),function (req,res){
+    let response = {
+        err:0,
+
+    };
+    for(var f of req.files){
+        var dir_file = __dirname+"\\"+f.originalname;
+        fs.readFile(f.path, function(err,data) {
+            fs.writeFile(dir_file, data ,function(err){
+                if(err){
+                    console.log("err")
+                    return response.err = 1;
+                }else{
+                    // response.data 
+                }
+
+            });
+            fs.stats(dir_file, function(err,stat){
+                if(err){
+                    console.log(err)
+                }else{
+                    console.log(stat)
+                }
+            })
+            
+        })
+    }
+
+    response["data"] = req.files;
     
+    res.send(response);
 })
-// app.post('/upload_imgs', function (req, res) {
-//     console.log(req.files[0]);  // 上传的文件信息
-//     var des_file = __dirname + "/" + req.files[0].originalname;
-//     fs.readFile(req.files[0].path, function (err, data) {
-//         fs.writeFile(des_file, data, function (err) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 response = {
-//                     message: 'File uploaded successfully',
-//                     filename: req.files[0].originalname
-//                 };
-//             }
-//             console.log(response);
-//             res.end(JSON.stringify(response));
-//         });
-//     });
-// })
 
 var server = app.listen("8081",function(){
     var host = server.address().address;
